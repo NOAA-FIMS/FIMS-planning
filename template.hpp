3ad637a70c22fd992afe044266df013d6277a375
Matthew-Supernaw-NOAA <matthew.supernaw@noaa.gov>
2021-12-03 10:56:57 -0500

    Update template.hpp

--- template.hpp
+++ template.hpp
@@ -1,71 +1,120 @@
-//  name.hpp
+//  tmplate.hpp
 //  Fisheries Integrated Modeling System (FIMS)
 
-#ifndef name_h
-#define name_h
+#ifndef template_hpp
+#define template_hpp
 
+//inherit from model_base
+#include "../common.hpp" 
 #include <map>
 #include <vector>
+#include <iostream>
+
+
 /*
  * name namespace
  */
 namespace fims{
 
 /**
- * name base class. Inherits from modelbase.
+ * NameBase class. Inherits from modelbase.
  */
 template <class T>
 
-class name_base:public model_base<T>{
+class NameBase: public model_base<T>{
 protected:
 
 public:
-  virtual T evaluate(const T& i)=0;
+  virtual T evaluate(const T& t)=0; //"= 0;" means this must be implemented in child
 };
   
-  template <class T>
-/* template class implements name_base
- * 
- * This is the default constructor.
- * 
- */
-class Name: public NameBase<T>{
-
-  std::vector<std::map<T, T> > local_variable;
+/* 
+* Template class inherits from  NameBase
+*/
+template <class T>
+class Name1: public NameBase<T>{
 
 public:
-  std::map<T, T> local_variable;
-  
-  enum NAME_TYPE{
-    OPTION1=0,
-    OPTION2
-  }
+ 
     
-    /*
+  /*
    *Default constructor
+   *Initialize any memory here.
    */
-  name(){
+  Name1(){
   }
   
  
   /**
-   * Destructor; this method deleted the von Bertalanffy growth object.
+   * Destructor; this method destructs Name1 object.
+   * Delete any allocated memory here.
    */
-  ~ name(){
-    std::cout <<"I just deleted name object" << std::endl;
+  ~ Name1(){
+    std::cout <<"I just deleted Name1 object" << std::endl;
   }
   
-   virtual T evaluate(const T& i, NAME_TYPE t) {
-     return this->name[t][i];
+   /**
+    * Note: this function must have the same signature as evaluate in NameBase.
+    * Overloaded virtual function. This is polymorphism, meaning the 
+    * signature has the same appearance, but the function itself has unique logic.
+    * 
+    * @param t
+    * @return t+1
+    */
+   virtual T evaluate(const T& t) {
+     std::cout<<"evaluate in Name1 received "<<t<<"as a method parameter, returning "<<(t+1)<<std::endl;
+     return t+1; //unique logic to Name1 class
    }
 
 };
+
+  /* 
+* Template class inherits from  NameBase
+*/
+template <class T>
+class Name2: public NameBase<T>{
+
+public:
+ 
+    
+  /*
+   *Default constructor.
+   *Initialize any memory here.
+   */
+  Name2(){
+  }
+  
+ 
+  /**
+   * Destructor; this method destructs the Name2 object.
+   * Delete any allocated memory here.
+   */
+  ~ Name2(){
+    std::cout <<"I just deleted Name2 object" << std::endl;
+  }
+  
+   /**
+    * Note: this function must have the same signature as evaluate in NameBase.
+    * Overloaded virtual function. This is polymorphism, meaning the 
+    * signature has the same appearance, but the function itself has unique logic.
+    * 
+    * @param t
+    * @return t^2
+    */
+   virtual T evaluate(const T& t) {
+     std::cout<<"evaluate in Name2 received "<<t<<"as a method parameter, returning "<<(t*t)<<std::endl;
+     return t*t; //unique logic to Name2 class
+   }
+
+};
+  
 /**
- *If there is only one implementation of this name type, the file can end here. Add secondary implementations below.
+ * Add additional implementations below.
  */
+  
 
 
 
-} /end namespace
+} //end namespace
 
-#endif /*name_h */
+#endif /*template_hpp */
